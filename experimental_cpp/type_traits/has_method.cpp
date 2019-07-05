1547e824c6647d8508e526b8277c1cc94c9964a4
Vaiz <vaizerd@gmail.com>
2019-07-05 23:01:23 +0200

    [has_method] (try) check method parameters

--- experimental_cpp/type_traits/has_method.cpp
+++ experimental_cpp/type_traits/has_method.cpp
@@ -1,4 +1,5 @@
 #include "has_method.h"
+#include <string>
 
 using namespace experimental;
 
@@ -8,6 +9,7 @@ struct StructWithoutMethod {};
 
 static_assert(!has_method_to_str_v<StructWithoutMethod>);
 static_assert(!has_method_to_str_ex_v<StructWithoutMethod>);
+static_assert(!has_method_to_str_ex_ex_v<StructWithoutMethod, const char*>);
 
 
 struct StructWithMethod {
@@ -16,6 +18,7 @@ struct StructWithMethod {
 
 static_assert(has_method_to_str_v<StructWithMethod>);
 static_assert(has_method_to_str_ex_v<StructWithMethod>);
+static_assert(has_method_to_str_ex_ex_v<StructWithMethod, const char*>);
 
 
 struct StructWithStaticMethod {
@@ -24,14 +27,27 @@ struct StructWithStaticMethod {
 
 static_assert(has_method_to_str_v<StructWithStaticMethod>);
 static_assert(has_method_to_str_ex_v<StructWithStaticMethod>);
+static_assert(has_method_to_str_ex_ex_v<StructWithStaticMethod, const char*>);
 
 
 struct StructWithWrongMethod {
     int to_str() const { return 1; }
+    const char* to_str(int) const { return ""; }
+    const char* to_str(double) const = delete; // double -> int :(
+    const char* to_str(int, std::string) const { return ""; }
 };
 
 static_assert(has_method_to_str_v<StructWithWrongMethod>);
 static_assert(!has_method_to_str_ex_v<StructWithWrongMethod>);
 static_assert(has_method_to_str_ex_v<StructWithWrongMethod, int>);
+
+static_assert(!has_method_to_str_ex_ex_v<StructWithWrongMethod, const char*>);
+static_assert(has_method_to_str_ex_ex_v<StructWithWrongMethod, const char*, int>);
+static_assert(has_method_to_str_ex_ex_v<StructWithWrongMethod, const char*, bool>);    // :(
+static_assert(!has_method_to_str_ex_ex_v<StructWithWrongMethod, const char*, double>); // :(
+static_assert(!has_method_to_str_ex_ex_v<StructWithWrongMethod, const char*, std::string>);
+static_assert(has_method_to_str_ex_ex_v<StructWithWrongMethod, const char*, int, std::string>);
+static_assert(!has_method_to_str_ex_ex_v<StructWithWrongMethod, const char*, std::string, std::string>);
+
 
 }
--- experimental_cpp/type_traits/has_method.h
+++ experimental_cpp/type_traits/has_method.h
@@ -18,9 +18,21 @@ template <typename T, typename TReturnType = const char*, typename = std::void_t
 struct has_method_to_str_ex : std::false_type {};
 
 template <typename T, typename TReturnType>
-struct has_method_to_str_ex<T, TReturnType, std::void_t<is_same_t<decltype(T().to_str()), TReturnType>>> : std::true_type {};
+struct has_method_to_str_ex<T, TReturnType, std::void_t<is_same_t<TReturnType, decltype(T().to_str())>>> : std::true_type {};
 
 template <typename T, typename TReturnType = const char*>
 constexpr bool has_method_to_str_ex_v = has_method_to_str_ex<T, TReturnType>::value;
+
+
+// still don't know how to pass method name :D
+template <typename T, typename TReturnType, typename... TMethodArgs>
+struct has_method_to_str_ex_ex : std::false_type {};
+
+// pass return type twice to fix C2764 : template parameter not used or deducible in partial specialization 'specialization'
+template <typename T, typename TReturnType, typename... TMethodArgs>
+struct has_method_to_str_ex_ex<T, TReturnType, is_same_t<TReturnType, decltype(T().to_str(std::declval<TMethodArgs>()...))>, TMethodArgs...> : std::true_type {};
+
+template <typename T, typename TReturnType, typename... TMethodArgs>
+constexpr bool has_method_to_str_ex_ex_v = has_method_to_str_ex_ex<T, TReturnType, TReturnType, TMethodArgs...>::value;
 
 }
